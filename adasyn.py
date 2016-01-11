0597e0ce37cb3c76034c5310ccae97b9b1fad0e6
stavskal <stavrianos_@hotmail.com>
2016-01-11 15:33:38 +0100

    ADASYN v0.1 complete

--- adasyn.py
+++ adasyn.py
@@ -6,28 +6,68 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.utils import check_array, check_random_state
 from collections import Counter
+
 __author__ = 'stavrianos'
 
 # Link to paper:  bit.ly/22KgAnP
 
-class Adasyn(object):
+class ADASYN(object):
     """
     Oversampling parent class with the main methods required by scikit-learn:
     fit, transform and fit_transform
     """
 
     def __init__(self,
-                 binary=False,
                  ratio=0.5,
                  imb_threshold=0.5,
                  k=5,
                  random_state=None,
                  verbose=True):
         """
-        Initialize Adasyn object with required parameters
+        :ratio:
+            Growth percentage with respect to initial minority
+            class size. For example if ratio=0.65 then after 
+            resampling minority class(es) will have 1.65 times
+            its initial size
+
+
+        :imb_threshold:
+            The imbalance ratio threshold to allow/deny oversampling.
+            For example if imb_threshold=0.5 then minority class needs
+            to be at most half the size of the majority in order for 
+            resampling to apply
+
+        :k:
+            Number of K-nearest-neighbors
+
+        :random_state:
+            seed for random number generation
+
+        :verbose:
+            Determines if messages will be printed to terminal or not
+
+        Extra Instance variables:
+
+        :self.X:
+            Feature matrix to be oversampled
+
+        :self.y:
+            Class labels for data
+
+        :self.clstats:
+            Class populations to determine minority/majority
+
+        :self.unique_classes_:
+            Number of unique classes
+
+        :self.maj_class_:
+            Label of majority class
+
+        :self.random_state_:
+            Seed
         """
 
-        self.binary = binary
+
         self.ratio = ratio
         self.imb_threshold = imb_threshold
         self.k = k
@@ -47,7 +87,6 @@ class Adasyn(object):
         self.random_state_ = check_random_state(self.random_state)
         self.unique_classes_ = set(self.y)
 
-        
         # Initialize all class populations with zero
         for element in self.unique_classes_:
             self.clstats[element] = 0
@@ -72,7 +111,7 @@ class Adasyn(object):
     	Applies oversampling transformation to data as proposed by
     	the ADASYN algorithm. Returns oversampled X,y
     	"""
-    	self.new_X , self.new_Y = self.oversample()
+    	self.new_X , self.new_y = self.oversample()
 
 
     def fit_transform(self,X,y):
@@ -80,40 +119,41 @@ class Adasyn(object):
     	Fits the data and then returns the transformed version
     	"""
     	self.fit(X,y)
-    	self.new_X, self.new_Y = self.oversample()
+    	self.new_X, self.new_y = self.oversample()
 
-    	return self.new_X, self.new_Y
+        self.new_X = np.concatenate((self.new_X,self.X),axis=0)
+        self.new_y = np.concatenate((self.new_y,self.y),axis=0)
+
+    	return self.new_X, self.new_y
 
     def generate_samples(self,x,knns,knnLabels,cl):
-        print('MY GI JOE')
-        print(sum(self.gi))
-        print(self.gi)
-        # Matrix to store synthetically generated samples
+
+        # List to store synthetically generated samples and their labels
         new_data = []
+        new_labels=[]
         for ind,elem in enumerate(x):
             # calculating k-neighbors that belong to minority (their indexes in x)
-            print(knns[ind][1:-1],knnLabels[ind])
             # Unfortunately knn returns the example itself as a neighbor. So it needs
             # to be ignored thats why it is iterated [1:-1] and knnLabelsp[ind][+1]. 
             min_knns = [ele for index,ele in enumerate(knns[ind][1:-1]) if knnLabels[ind][index+1]==cl]
             if not min_knns:
-                print('No k-neighbors belong to the minority class for row %s ,no samples produced' % elem)
+                if self.verbose:
+                    print('No k-neighbors belong to the minority class for row %s ,no samples produced' % elem)
                 continue
 
             # generate gi synthetic examples for every minority example
             for i in range(0,int(self.gi[ind])):
-                # randi holds an integer to choose from minority kNNs
+                # randi holds an integer to choose a random minority kNNs
                 randi=self.random_state_.random_integers(0,len(min_knns)-1)
+                # l is a random number in [0,1)
                 l = self.random_state_.random_sample()
                 #X[min_knns[randi]] is the Xzi on equation [5]
                 si = self.X[elem] + (self.X[min_knns[randi]]-self.X[elem])*l
                 new_data.append(si)
-                
+                new_labels.append(self.y[elem])
 
-                print('Synthetic example: %s ' % si)
-                print('Produced by initial: %s %s' % (self.X[elem], self.y[elem]))
-                print('And brother:         %s %s' % (self.X[min_knns[randi]],self.y[min_knns[randi]]))
-        print(np.asarray(new_data).shape)
+        
+        return(np.asarray(new_data),np.asarray(new_labels))
 
     def oversample(self):
         """
@@ -129,19 +169,8 @@ class Adasyn(object):
         except:
     		raise RuntimeError("You need to fit() before applying tranform(),"
     							"or simply fit_transform()")
-
-    	# Checking if parameters are set correctly depending
-        # on whether the labels are binary / multiclass
-    	if len(self.unique_classes_) == 1:
-            raise RuntimeError("Only one class present, exiting...")
-        elif len(self.unique_classes_) == 2:
-            if not self.binary:
-                raise ValueError('Set binary=True for binary classification')
-        else:
-            if self.binary:
-                raise ValueError('Set binary=False(default)for multiple '
-                                 'classes')
-        
+        int_X = np.zeros([1,self.X.shape[1]])
+        int_y = np.zeros([1])
         # Iterating through all minority classes to determine
         # if they should be oversampled and to what extent
     	for cl in self.unique_classes_:
@@ -151,7 +180,6 @@ class Adasyn(object):
             	if self.verbose:
             		print('Class %s is within imbalance threshold' % cl)
             else:
-                print('WE TALKING ABOUT CLASS %s' % cl)
                 # G is the number of synthetic examples to be synthetically
                 # produced for the current minority class
                 self.G = (self.clstats[self.maj_class_] - self.clstats[cl]) * self.ratio
@@ -161,17 +189,16 @@ class Adasyn(object):
 
                 # keeping indexes of minority examples
                 minx = [ind for ind,exam in enumerate(self.X) if y[ind]==cl]
-                #print(minx, self.y[minx])
+
                 # Computing kNearestNeighbors for every minority example
                 knn = self.nearest_neighbors_.kneighbors(self.X[minx],return_distance=False)
-               # print(y[knn])
+
                 # Getting labels of k-neighbors of each example to determine how many of them 
                 # are of different class than the one being oversampled
                 knnLabels = [y[ele] for ind,ele in enumerate(knn)]
 
 
                 tempdi = [Counter(i) for i in knnLabels]
-               # print(tempdi)
                 # Calculating ri as defined in ADASYN paper:
                 # No. of k-neighbors belonging to different class than the minority divided by K 
                 # which is ratio of friendly/non-friendly neighbors
@@ -185,13 +212,18 @@ class Adasyn(object):
                 # each minority instance and rounding to nearest integer because
                 # it can't produce e.g 2.35 new examples.
                 self.gi = np.rint(self.ri * self.G)
-              #  print(self.gi)
-                #inter_X,inter_Y = self.generate_samples(minx,knn,knnLabels,cl)
                 
-                self.generate_samples(minx,knn,knnLabels,cl)
-                #print(self.random_state_.rand(1))
+                # Generation of synthetic samples
+                inter_X, inter_y = self.generate_samples(minx,knn,knnLabels,cl)
+                # in case no samples where generated at all concatenation
+                # won't be attempted
+                if len(inter_X):
+                    int_X = np.concatenate((int_X,inter_X),axis=0)
+                if len(inter_y):
+                    int_y = np.concatenate((int_y,inter_y),axis=0)
+                #print(inter_X,inter_y)
 
-        return(inter_X,inter_Y)
+        return(int_X[1:-1],int_y[1:-1])
 
 
 
@@ -227,8 +259,10 @@ data = load_iris()
 X = data['data']
 y = data['target']
 print(Counter(y))
-#X,y = deleteClass(X,y,30,0)
-X,y = deleteClass(X,y,28,2)
+X,y = deleteClass(X,y,15,1)
+#X,y = deleteClass(X,y,28,2)
+X,y = deleteClass(X,y,28,0)
+
 print(X.shape[1])
 print(Counter(y))
 #exit()
@@ -236,5 +270,8 @@ testnn = NearestNeighbors(n_neighbors=3, p=2).fit(X)
 b =testnn.kneighbors(X,return_distance=False)
 asda = ([y[ele] for ind,ele in enumerate(b) if y[ind]==1])
 
-a=Adasyn(k=8,verbose=True)
-a.fit_transform(X,y)
+a=ADASYN(k=10,imb_threshold=0.8,ratio=0.6,verbose=True)
+newx,newy=a.fit_transform(X,y)
+print('my last word')
+print(newx,newy)
+print(newx.shape,newy.shape)
